ac1e844be5c1c4a1731b3407b99c1c292482b2e5
Alessandro Perrone <perrone.1900516@studenti.uniroma1.it>
2025-09-24 13:00:59 +0200

    Add files via upload

--- radial_distribution_function.py
+++ radial_distribution_function.py
@@ -6,9 +6,10 @@ import numpy as np
 from numpy import zeros, array, pi, sum, sqrt
 import pandas as pd
 import os
+from scipy.optimize import curve_fit
 
 
-def dist_2D_periodic(pos1, pos2, box_size_2d):
+def dist(pos1, pos2, box_size_2d):
     """Compute periodic distance between two points in 2D (y, x)."""
     d = np.abs(pos1 - pos2)
     #for dim in range(1,2):
@@ -40,7 +41,7 @@ def rdf_2D(positions, box_size, dr):
             # Use only y and x components
             pos_i = positions[i][1:]  # [y, x]
             pos_j = positions[j][1:]
-            d = dist_2D_periodic(pos_i, pos_j, box_size_2d)
+            d = dist(pos_i, pos_j, box_size_2d)
             if d < r_max:
                 bin_idx = int(d / dr)
                 if bin_idx < len(count):
@@ -57,7 +58,14 @@ def rdf_2D(positions, box_size, dr):
 
 
 def rdf_2D_sliced(positions, box_size, dr,z_gap, slice_tolerance):
-    '''Computes the 2D g(r) by slicing the z stack according to the z resolution'''
+    '''Computes the 2D g(r) by slicing the z stack according to the z resolution
+    Takes: positions: array of shape (N, 3) in order [z, y, x]
+           box_size: [z, y, x]
+           dr: radial bin width
+           z_gap: gap between z slices
+           slice_tolerance: minimum number of particles in a slice to be considered valid
+    Returns: r_centers, g_r_averaged average g(r) over all valid slices
+    '''
     positions = np.asarray(positions)
     Np = len(positions)
     box_size_2d = box_size[1:]  # y, x
@@ -78,15 +86,14 @@ def rdf_2D_sliced(positions, box_size, dr,z_gap, slice_tolerance):
 
         mask = (z_vals >= z_lo) & (z_vals < z_hi) # this is boolean, gives False or True depending on the condition
         slices = positions[mask][:, 1:] # the first : is to slice colum-wise, then 1: takes the y,x coord
-        print(slices)
-        if len(slices) > slice_tolerance:
+        if len(slices) > slice_tolerance: # if there are enough particles in the slice
             rho_2d = len(slices) / (box_size_2d[0] * box_size_2d[1])
             count = np.zeros(Nbins)
             for k in range(len(slices)):
                 for l in range(k + 1, len(slices)):
                     pos_k = slices[k] 
                     pos_l = slices[l]
-                    d = dist_2D_periodic(pos_k, pos_l, box_size_2d)
+                    d = dist(pos_k, pos_l, box_size_2d)
                     if d < r_max:
                         bin_idx = int(d / dr)
                         if bin_idx < len(count):
@@ -130,7 +137,8 @@ def rdf_3D(positions, box_size, dr):
 
     for i in range(Np):
         for j in range(i + 1, Np):
-            d = dist_2D_periodic(positions[i], positions[j], box_size)
+            #d = dist_2D_periodic(positions[i], positions[j], box_size)
+            d = np.linalg.norm(positions[i] - positions[j])
             if d < r_max:
                 bin_idx = int(d / dr)
                 if bin_idx < len(count):
@@ -144,3 +152,69 @@ def rdf_3D(positions, box_size, dr):
         g_r[i] = count[i] / (rho * Np * shell_volume)
 
     return r_centers, g_r
+
+
+def rdf_3D_restricted(positions, box_size, dr, thr):
+    """
+    Computes the 3D g(r) excluding edge particles as RDF centers.
+    positions: array (N, 3) in order [z, y, x]
+    box_size: [z, y, x]
+    thr: tuple with (thr_z, thr_y, thr_x) defining excluded edge thickness
+    """
+    positions = np.asarray(positions)
+    Np = len(positions)
+    r_max = min(box_size) / 2
+    Nbins = int(r_max / dr)
+    count = np.zeros(Nbins)
+    r_edges = np.linspace(0, r_max, Nbins + 1)
+    r_centers = 0.5 * (r_edges[:-1] + r_edges[1:])
+    
+    thr_z, thr_y, thr_x = thr
+    
+    # Identify indices of "good" (non-edge) points
+    good_indices = []
+    for i in range(Np):
+        if (positions[i, 0] > thr_z and positions[i, 0] < (box_size[0] - thr_z) and
+            positions[i, 1] > thr_y and positions[i, 1] < (box_size[1] - thr_y) and
+            positions[i, 2] > thr_x and positions[i, 2] < (box_size[2] - thr_x)):
+            good_indices.append(i)
+    N_ref = len(good_indices)
+
+    # Number density
+    rho = Np / np.prod(box_size)
+
+    # Count pairs (only if i is a "good" center)
+    for i in good_indices:
+        for j in range(Np):
+            if i == j:
+                continue
+            d = np.linalg.norm(positions[i] - positions[j])
+            if d < r_max:
+                bin_idx = int(d / dr)
+                if bin_idx < len(count):
+                    count[bin_idx] += 1
+
+    # Normalize
+    g_r = np.zeros(Nbins)
+    for i in range(Nbins):
+        r_outer = r_edges[i+1]
+        r_inner = r_edges[i]
+        shell_volume = (4/3) * np.pi * (r_outer**3 - r_inner**3)
+        g_r[i] = count[i] / (rho * N_ref * shell_volume)
+
+    return r_centers, g_r
+
+
+def critical_func(x, A, l, eta):
+    """Critical function for fitting RDF peaks.
+    g(r) = A * (exp(-r/l))/r^(1+eta) + 1
+        A=amplitude l=correlation length eta=critical exponent
+    """
+    return A * (np.exp(-x/l))/np.power(x, 1+eta) + 1
+
+
+def fit_rdf_critical(start, rdf, dists):
+    """Fit the RDF using the critical function."""
+    popt, pcov = curve_fit(critical_func, dists[start:], rdf[start:], p0=[1, 1, 1])
+    return popt, pcov
+
